5fd39d8451554a0dcba4db09cb3200d5e005dc26
Mario de Luna <lunacreado@gmail.com>
2015-05-12 19:22:17 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function visualChng(x){
+	x.style.backgroundColor = "silver";
+	x.style.color = "black";
+	x.style.textAlign = "center";
+}
+
+function visualBck(x){
+	x.style.backgroundColor = "#27226F";
+	x.style.color = "white";
+	x.style.textAlign = "left";
+}
